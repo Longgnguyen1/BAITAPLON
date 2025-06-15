3a31d5b62dffb5b78ca33c90d170fed447582c5d
Longgnguyen1 <137403685+Longgnguyen1@users.noreply.github.com>
2025-06-15 21:55:26 +0700

    Delete Baitaplon directory

--- Baitaplon/configs/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('_WEB_HOST', 'http://'. $_SERVER['HTTP_HOST'] .'/baitaplon');
-
-//database
-const _HOST = 'localhost';
-const _DB = 'baitaplon';
-const _USER = 'root';
-const _PASS = '';
